8651fc02b43552ab266648e136892fe6cfaca50e
maciekmacku <maciejszalaj@gmail.com>
2018-04-26 22:24:22 +0200

    added html and scripts

---
+++ js/script.js
@@ -0,0 +1,46 @@
+var id = 0;
+var phoneList = [];
+var addBtn = document.getElementById('add-phone');
+
+// helper functions
+function generateID() {
+	return ++id;
+}
+function validateFields(brand, model, price) {
+	if (brand.length > 0  &&  model.length > 0 && price.length >0){
+		return true;
+	} else {
+		return false;
+	}
+	
+}
+// constructor function
+function Phone(brand, model, price) {
+	this.brand = brand;
+	this.model = model;
+	this.price = price;
+	this.id = generateID();
+	
+}
+
+// click event
+addBtn.addEventListener('click', function(){
+	var brand = document.getElementById('brand').value;
+	var model = document.getElementById('model').value;
+	var price = document.getElementById('price').value;
+	var isValid = validateFields(brand, model, price);
+	console.log(isValid);
+	
+	if (isValid == true ){
+		var newPhone = new Phone(brand, model, price);
+		console.log(newPhone);
+		phoneList.push(newPhone);
+		console.log(phoneList);
+	}
+	
+});
+
+
+
+
+	
